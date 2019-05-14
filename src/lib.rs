e113785ef827da1f35444a7423aea36c118c0de0
Jason Kim <actcrazy@gmail.com>
2019-05-14 16:23:14 +0900

    feat: search_case_insensitive 추가

--- src/lib.rs
+++ src/lib.rs
@@ -31,6 +31,19 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
